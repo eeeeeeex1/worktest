a5269b33ff59bde6ab75c97de0806ac0d5279576
eeeeeeex1 <2629620478@qq.com>
2026-03-16 15:52:36 +0800

    main分支：修改hello.py，添加主分支标识

--- # 简单的问候程序.py
+++ # 简单的问候程序.py
@@ -4,6 +4,11 @@ name = input("请输入你的名字：")
 
 # 输出个性化问候
 print(f"你好，{name}！👋 欢迎学习 Python 和 GitHub！")
-
-# 额外输出一行提示，增加趣味性
+ 
+# 额外输出一行提示，增加趣味性  main测试
+print(f"{name}，这是你提交到 GitHub 的第一个 Python 代码哦～")
+print(f"{name}，这是你提交到 GitHub 的第一个 Python 代码哦～")
+print(f"{name}，这是你提交到 GitHub 的第一个 Python 代码哦～")
+print(f"{name}，这是你提交到 GitHub 的第一个 Python 代码哦～")
+print(f"{name}，这是你提交到 GitHub 的第一个 Python 代码哦～")
 print(f"{name}，这是你提交到 GitHub 的第一个 Python 代码哦～")
